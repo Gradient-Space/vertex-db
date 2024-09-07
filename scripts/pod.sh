95a21be700ef85e54e50165e352a6f1a279847c7
c3n0te <tyellison05@hotmail.com>
2024-09-07 15:29:33 -0700

    Update

--- scripts/pod.sh
+++ scripts/pod.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 source .env
-podman pod create --name Vertex -p 5432:5432/TCP -p 50051:50051/TCP -p 3000:3000/TCP 
-podman build -f Containerfile --rm -t localhost/vertex-db:v0.1 --no-cache
-podman run -d --rm --pod Vertex --name vertex-db -e POSTGRES_PASSWORD=postgres localhost/vertex-db:v0.1
+podman pod create --name Vertex -p 5432:5432/TCP -p 50051:50051/TCP -p 4000:4000/TCP -p 3000:3000/TCP 
+podman build -f Containerfile --rm -t localhost/vertex-db:v0.1 
+
+podman run -d --rm \
+    --pod Vertex \
+    --name vertex-db \
+    -e POSTGRES_PASSWORD=${DB_PASS} \
+    localhost/vertex-db:v0.1
